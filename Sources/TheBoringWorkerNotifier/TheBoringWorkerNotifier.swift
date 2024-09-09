dbc1bc8d9b4fd44d3a09e1036e3aa3bf1cd7b3ac
iamharshdev <me@iamharsh.dev>
2024-09-09 15:47:46 +0530

    Intial build of TheBoringWorkerNotifier

--- Sources/TheBoringWorkerNotifier/TheBoringWorkerNotifier.swift
+++ Sources/TheBoringWorkerNotifier/TheBoringWorkerNotifier.swift
@@ -1,2 +1,48 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+
+import Foundation
+
+struct WorkerNotification {
+    var name: String
+    var handler: (Notification) -> Void
+}
+
+class TheBoringWorkerNotifier {
+    private let notificationCenter = DistributedNotificationCenter.default()
+    
+    let toggleMicNotification: WorkerNotification = .init(name: "theboringteam.theboringworker.togglemic", handler: { _ in
+        print("Received toggle mic notification")
+    })
+    
+    let toggleHudReplacementNotification: WorkerNotification = .init(name: "theboringteam.theboringworker.togglehudreplacement", handler: { _ in
+        print("Received toggle hud replacement notification")
+    })
+    
+    let showClipboardNotification: WorkerNotification = .init(name: "theboringteam.theboringnotch.showclipboard", handler: { _ in
+        
+        print("Received show clipboard notification")
+    })
+    
+    let sneakPeakNotification: WorkerNotification = .init(name: "theboringteam.workers.sneakPeak", handler: { _ in
+        print("Received sneak peak notification")
+    })
+    
+    let micStatusNotification: WorkerNotification = .init(name: "theboringteam.theboringworker.micstatus", handler: { _ in
+        print("Received mic status notification")
+    })
+
+    init() {
+        NSLog("TheBoringWorkerNotifier init")
+    }
+    
+    func postNotification(name: String, userInfo: [AnyHashable: Any]?) {
+        DispatchQueue.main.async {
+            self.notificationCenter.postNotificationName(.init(name), object: nil, userInfo: userInfo, deliverImmediately: true)
+        }
+    }
+    
+    func setupObserver(notification: WorkerNotification, handler: @escaping (Notification) -> Void) {
+        notificationCenter.addObserver(forName: .init(notification.name), object: nil, queue: nil, using: handler)
+    }
+}
--- Tests/TheBoringWorkerNotifierTests/TheBoringWorkerNotifierTests.swift
+++ Tests/TheBoringWorkerNotifierTests/TheBoringWorkerNotifierTests.swift
@@ -2,11 +2,64 @@ import XCTest
 @testable import TheBoringWorkerNotifier
 
 final class TheBoringWorkerNotifierTests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    var notifier: TheBoringWorkerNotifier!
+    
+    override func setUp() {
+        super.setUp()
+        notifier = TheBoringWorkerNotifier()
+    }
+    
+    override func tearDown() {
+        notifier = nil
+        super.tearDown()
+    }
+    
+    func testInitialization() {
+        XCTAssertNotNil(notifier)
+    }
+    
+    func testPostNotification() {
+        let expectation = XCTestExpectation(description: "Notification posted")
+        
+        notifier.setupObserver(notification: notifier.toggleMicNotification) { notification in
+            XCTAssertEqual(notification.name.rawValue, "theboringteam.theboringworker.togglemic")
+            XCTAssertEqual(notification.userInfo?["test"] as? String, "value")
+            expectation.fulfill()
+        }
+        
+        notifier.postNotification(name: "theboringteam.theboringworker.togglemic", userInfo: ["test": "value"])
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testSetupObserver() {
+        let expectation = XCTestExpectation(description: "Observer setup and notification received")
+        
+        notifier.setupObserver(notification: notifier.toggleHudReplacementNotification) { notification in
+            XCTAssertEqual(notification.name.rawValue, "theboringteam.theboringworker.togglehudreplacement")
+            expectation.fulfill()
+        }
+        
+        notifier.postNotification(name: "theboringteam.theboringworker.togglehudreplacement", userInfo: nil)
+        
+        wait(for: [expectation], timeout: 1.0)
+    }
+    
+    func testMultipleNotifications() {
+        let expectation1 = XCTestExpectation(description: "First notification received")
+        let expectation2 = XCTestExpectation(description: "Second notification received")
+        
+        notifier.setupObserver(notification: notifier.showClipboardNotification) { _ in
+            expectation1.fulfill()
+        }
+        
+        notifier.setupObserver(notification: notifier.sneakPeakNotification) { _ in
+            expectation2.fulfill()
+        }
+        
+        notifier.postNotification(name: "theboringteam.theboringnotch.showclipboard", userInfo: nil)
+        notifier.postNotification(name: "theboringteam.workers.sneakPeak", userInfo: nil)
+        
+        wait(for: [expectation1, expectation2], timeout: 1.0)
     }
 }
